8a833d14ae7de4e4534b4f5b56483c9168fa2e37
martintisco <32553715+martintisco@users.noreply.github.com>
2017-10-25 10:47:12 -0300

    Cambios en SCRIPT 2
    Universidad:
    -cambios en attr nombre
    -add telefono como not null
    
    Investigador:
    -cambios attr idInvestigador
    -add attr idUniversidad
    -cambios chk nivelInvestig
    -mail como unique
    
    Trabajo SIN CAMBIOS
    
    TAGS SIN CAMBIOS
    
    TTags:
    - cambios attr idTrab
    - FK idTag
    -FK idTRab
    - PK (idTrab, idTag)
    
    TAutores:
    - cambios attr idTrab
    -cambios nombres constraint
    
    Lugares:
    -cambios attr nombre
    - check nivelLugar, faltaba 'L'
    -FK de attr universidad (nombre)

--- Script2.sql
+++ Script2.sql
@@ -1,15 +1,14 @@
 BEGIN TRANSACTION
 
 /*      UNIVERSIDAD       */
-
 ALTER TABLE Universidad
-	ALTER COLUMN nombre INTEGERNOT NULL
+	DROP COLUMN nombre;
 GO
 ALTER TABLE Universidad
-	ADD CONSTRAINT nombre_Pk PRIMARY KEY (nombre);
+	ADD nombre VARCHAR (50) PRIMARY KEY;
 GO
 ALTER TABLE Universidad
-    ADD telefono VARCHAR(20);
+    ADD telefono VARCHAR(30) NOT NULL;
 GO
 	
 /*     INVESTIGADOR     */
@@ -21,24 +20,37 @@ ALTER TABLE Investigador
 	ADD  idInvestigador int IDENTITY(1,1) PRIMARY KEY;
 GO
 ALTER TABLE Investigador
-	ADD CONSTRAINT CK_nivelInvestig CHECK (nivelInvestig IN ('EGrado','EMaestria','EDoctor','Doctor'));
+	ADD  idUniversidad VARCHAR (50) FOREIGN KEY (idUniversidad) 
+	REFERENCES Universidad(nombre) NOT NULL;
 GO
 ALTER TABLE Investigador
-	ADD CONSTRAINT mail_UQ UNIQUE (mail);
+	ADD CONSTRAINT CK_nivelInvestig_Investigador CHECK (nivelInvestig IN ('EGrado','EMaestria','EDoctor','Doctor'));
+GO
+ALTER TABLE Investigador
+	ADD CONSTRAINT UQ_mail_Investigador UNIQUE (mail);
 GO
 	
 /*      TRABAJO     */
+
+--idTrab debe modificarse mas adelante. Su PK es un alfanumerico con condiciones
+ALTER TABLE Trabajo
+    DROP COLUMN idTrab;
+GO
+ALTER TABLE Trabajo
+    ADD idTrab VARCHAR(20) PRIMARY KEY;
+GO
+------------------------------------------------------------------------------
 ALTER TABLE Trabajo
     DROP COLUMN descripTrab;
 GO
 ALTER TABLE Trabajo
-    ADD descripTrab VARCHAR(200)NOT NULL;
+    ADD descripTrab VARCHAR(200) NOT NULL;
 GO
 ALTER Table Trabajo
-	ADD CONSTRAINT CK_tipoTrab CHECK (tipoTrab IN ('poster','articulo','capitulo','otro'));
+	ADD CONSTRAINT CK_tipoTrab_Trabajo CHECK (tipoTrab IN ('poster','articulo','capitulo','otro'));
 GO
 ALTER TABLE Trabajo
-	ADD CONSTRAINT lugarPublic_Fk_Lugares FOREIGN KEY (lugarPublic)
+	ADD CONSTRAINT FK_lugarPublic_Lugares FOREIGN KEY (lugarPublic)
 	REFERENCES Lugares (idLugar);
 GO
 
@@ -52,15 +64,30 @@ GO
 	
 /*      TTags     */
 ALTER TABLE TTags
-	ADD CONSTRAINT idTag_Fk_Tags FOREIGN KEY (idTag)
+	DROP COLUMN idTrab;
+GO
+ALTER TABLE TTags
+	ADD  idTrab varchar (20) NOT NULL; --SE TENDRÁ QUE MODIFICAR idTrab SEGÚN CAMBIO EN TABLA TRABAJO
+GO
+ALTER TABLE TTags
+	ADD CONSTRAINT FK_idTag_TTags FOREIGN KEY (idTag)
 	REFERENCES Tags (idtag);
 GO
 ALTER TABLE TTags
-	ADD CONSTRAINT idTrab_Fk_Trabajo FOREIGN KEY (idTrab)
+	ADD CONSTRAINT FK_idTrab_TTags FOREIGN KEY (idTrab)
 	REFERENCES Trabajo (idTrab);
+GO
+ALTER TABLE TTags
+	ADD CONSTRAINT PK_idTag_idTrab_TTags PRIMARY KEY (idTrab,idTag)
 GO
 	
 /*    TAutores   */
+ALTER TABLE TAutores
+	DROP COLUMN idTrab;
+GO
+ALTER TABLE TAutores
+	ADD  idTrab varchar (20) NOT NULL; --SE TENDRÁ QUE MODIFICAR idTrab SEGÚN CAMBIO EN TABLA TRABAJO
+GO
 ALTER TABLE TAutores
     DROP COLUMN rolinvestig;
 GO
@@ -68,16 +95,36 @@ ALTER TABLE TAutores
 	ADD  rolinvestig VARCHAR (20);
 GO
 ALTER TABLE TAutores
-	ADD CONSTRAINT CK_rolinvestig CHECK (rolinvestig IN ('autor-ppal','autor-sec','autor-director'));
+	ADD CONSTRAINT CK_rolinvestig_TAutores CHECK (rolinvestig IN ('autor-ppal','autor-sec','autor-director'));
 GO
 ALTER TABLE TAutores
-	ADD CONSTRAINT idInvestigador_Fk_Investigador FOREIGN KEY (idInvestigador)
+	ADD CONSTRAINT FK_idTrab_TAutores FOREIGN KEY (idTrab)
+	REFERENCES Trabajo (idTrab);
+GO
+ALTER TABLE TAutores
+	ADD CONSTRAINT FK_idInvestigador_TAutores FOREIGN KEY (idInvestigador)
 	REFERENCES Investigador (idInvestigador);
+GO
+ALTER TABLE TAutores
+	ADD CONSTRAINT PK_idTrab_idInvestigador_TAutores PRIMARY KEY (idTrab, idInvestigador); --ATENCIÓN CON idTrab SEGÚN CAMBIO EN TABLA TRABAJO
 GO
 	
 /*     Referencias    */
 ALTER TABLE Referencias
-	ADD CONSTRAINT CK_idTrab CHECK (idTrab <> idTrabReferenciado);
+	DROP COLUMN idTrab;
+GO
+ALTER TABLE Referencias
+	ADD  idTrab varchar (20) NOT NULL; --SE TENDRÁ QUE MODIFICAR idTrab SEGÚN CAMBIO EN TABLA TRABAJO
+GO
+ALTER TABLE Referencias
+	ADD CONSTRAINT CK_idTrab_Referencias CHECK (idTrab <> idTrabReferenciado);
+GO
+ALTER TABLE Referencias
+	ADD CONSTRAINT FK_idTrab_Referencias FOREIGN KEY (idTrab)
+	REFERENCES Trabajo (idTrab);
+GO
+ALTER TABLE Referencias
+	ADD CONSTRAINT PK_idTrab_idTrabReferenciado_Referencias PRIMARY KEY (idTrab, idTrabReferenciado); --ATENCIÓN CON idTrab SEGÚN CAMBIO EN TABLA TRABAJO
 GO
 
 /*   Lugares   */
@@ -88,16 +135,19 @@ ALTER TABLE Lugares
     DROP COLUMN nombre;
 GO
 ALTER TABLE Lugares
-	ADD  nombre VARCHAR(250);
+	ADD  nombre VARCHAR(250) NOT NULL;
 GO
 ALTER TABLE Lugares
-	ADD CONSTRAINT nombre_UQ Unique (nombre);
+	ADD CONSTRAINT UQ_nombre_Lugares UNIQUE (nombre);
 GO
 ALTER TABLE Lugares
-	ADD CONSTRAINT CK_TipoLugar CHECK (TipoLugar IN ('Congresos','Revistas','Libros'));
+	ADD CONSTRAINT CK_TipoLugar_Lugares CHECK (TipoLugar IN ('Congresos','Revistas','Libros'));
 GO
 ALTER TABLE Lugares
-	ADD CONSTRAINT CK_nivelLugar CHECK (niveLugar BETWEEN 1 AND 4);
+	ADD CONSTRAINT CK_nivelLugar_Lugares CHECK (nivelLugar BETWEEN 1 AND 4);
+GO
+ALTER TABLE Lugares
+	ADD CONSTRAINT FK_universidad_Lugares FOREIGN KEY (universidad) REFERENCES Universidad (nombre);
 GO
 
 
